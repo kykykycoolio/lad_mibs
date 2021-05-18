f57473ff72509b52b09b593f60af7e1026080f67
Jameson.Brown <jameson.brown@mx.com>
2021-05-18 17:14:40 -0600

    Updated logo from class to id and added test buttons and JS
    Co-authored-by: kykykycoolio <kykykycoolio@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,12 @@
+// Selectors
+const header = document.getElementsByClassName("header")[0];
+
+function goToGoogle() {
+  alert("Logo was clicked");
+}
+function changeHeaderPink() {
+  header.style.background = "pink";
+}
+function changeHeaderYellow() {
+  header.style.background = "yellow";
+}
